8d3554e59c6ea997a97770809e14954cc4e114c9
Gabriel Amaral <gabriel.amaralll1995@gmail.com>
2023-04-27 18:18:09 -0300

    requisito 2 cobrindo certo dessa vez

--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -1,71 +1,38 @@
-// const sinon = require('sinon');
-// const { expect } = require('chai') 
-
-// const { productsMockFromDB } = require('./productsMockFromDB');
-
-// const { productService } = require('../../../src/services');
-
-// const { productModel } = require('../../../src/models');
-// const e = require('express');
-
-// describe("test service products", () => {
-//   describe("Testa quando tem produtos no banco", () => {
-//     it("testa a busca por todos os produtos no banco", async () => {
-//       sinon.stub(productModel, "getAllProd").resolves(productsMockFromDB);
-//       const result = await productService.getAllController();
-//       expect(result).to.deep.equal(productsMockFromDB);
-//     });
-
-//     it("Testa a busca por um produto no banco", async () => {
-//       sinon.stub(productModel, "getByIdProd").resolves(productsMockFromDB);
-//       const result = await productService.getByIdController(2);
-//       expect(result).to.deep.equal(productsMockFromDB[2]);
-//     });
-
-//     it("Testa a inserção de um produto no banco", async () => {
-//       sinon.stub(productModel, "createProd").resolves({ insertId: 4 });
-//       const result = await productService.createService(
-//         "Armadura do Homem de Ferro"
-//       );
-//       expect(result).to.deep.equal(newProduct);
-//     });
-//     afterEach(sinon.restore);
-// });
-// });
-
 const sinon = require('sinon');
-const { expect } = require('chai');
-
+const { expect } = require('chai') 
 const { productsMockFromDB } = require('./productsMockFromDB');
-
 const { productService } = require('../../../src/services');
-
 const { productModel } = require('../../../src/models');
-const e = require('express');
 
-describe("test service products", () => {
-  describe("Testa quando tem produtos no banco", () => {
-    afterEach(() => {
+describe('product service layer', function () {
+    afterEach(function () {
       sinon.restore();
     });
 
-    it("testa a busca por todos os produtos no banco", async () => {
-      sinon.stub(productModel, "getAllProd").resolves(productsMockFromDB);
-      const result = await productService.getAllController();
-      expect(result).to.deep.equal(productsMockFromDB);
-    });
+  describe("test service products", function () {
+    describe("Testa quando tem produtos no banco", function () {
+      it("testa a busca por todos os produtos no banco", async function () {
+        sinon.stub(productModel, "getAllController").resolves(productsMockFromDB);
+        
+        const result = await productService.getAllController();
+        expect(result).to.deep.equal(productsMockFromDB);
+      });
+      describe('busca de um produto', function () {
+        it("Testa a busca por um produto no banco", async function () {
+          sinon.stub(productModel, "getByIdController").resolves(productsMockFromDB[0]);
 
-    it("Testa a busca por um produto no banco", async () => {
-      sinon.stub(productModel, "getByIdProd").resolves(productsMockFromDB[2]);
-      const result = await productService.getByIdController(2);
-      expect(result).to.deep.equal(productsMockFromDB[2]);
-    });
+          const result = await productService.getByIdController(1);
+          expect(result).to.be.deep.equal(productsMockFromDB[0]);
+        });
 
-    it("Testa a inserção de um produto no banco", async () => {
-      const newProduct = { id: 4, name: "Armadura do Homem de Ferro" };
-      sinon.stub(productModel, "createProd").resolves({ insertId: 4 });
-      const result = await productService.createService(newProduct.name);
-      expect(result).to.deep.equal(newProduct);
+        // it("Testa a inserção de um produto no banco", async function () {
+        //   sinon.stub(productModel, "createProd").resolves({ insertId: 4 });
+        //   const result = await productService.createService(
+        //     "Armadura do Homem de Ferro"
+        //   );
+        //   expect(result).to.deep.equal([productsMockFromDB[4]]);
+        // });
+      });
     });
   });
-});
+  });
